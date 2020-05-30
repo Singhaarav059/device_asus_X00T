f8c01a4b32516299d15801d03a2bfe36685f033a
Singhaarav059 <singhaarav059@gmail.com>
2020-05-30 02:43:23 +0000

    disable vilte for all operator enable only for jio as only jio supports vilte in india

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -43,7 +43,6 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.sib16_support=1 \
     persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1 \
     qemu.hw.mainkeys=0 \
     rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
